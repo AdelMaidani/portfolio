141cb59f68f61506cb594dd28d3992891ec96cf9
Mohammed Adel Maidani <adelmaidani12@gmail.com>
2022-12-09 11:49:53 +0530

    Adding portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates sit
+      delectus quibusdam doloribus, eaque eum, odio quos assumenda aliquid
+      architecto voluptatem est hic facere recusandae, asperiores fugiat nobis.
+      Perferendis, soluta!
+    </div>
+  );
+}
+
+export default App;
